3c13588b21265a02b9df18907a6a248db0555e07
chloethemeowcat-gif <chloethemeowcat@gmail.com>
2026-06-24 07:09:17 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 module.exports = {
 nodes: [
     {
-        host: "lavalink-v4.triniumhost.com",
-        password: "https://discord.gg/CxsBjs5RAU",
+        host: "lava-v4.ajieblogs.eu.org",
+        password: "https://dsc.gg/ajidevserver",
         port: 443,
         secure: true,
         name: "Main"
     },
     {
-        host: "lava-v4.millohost.my.id",
-        password: "https://discord.gg/CxsBjs5RAU",
+        host: "lavalinkv4.serenetia.com",
+        password: "https://dsc.gg/ajidevserver",
         port: 443,
         secure: true,
         name: "Serenetia"
     },
     {
         host: "lavalink.jirayu.net",
-        password: "https://discord.gg/CxsBjs5RAU",
+        password: "youshallnotpass",
         port: 443,
         secure: true,
         name: "Jirayu"
     },
     {
-        host: "lavalinkv4.serenetia.com",
-        password: "https://discord.gg/CxsBjs5RAU",
+        host: "lava-v4.millohost.my.id",
+        password: "https://discord.gg/mjS5J2K3ep",
         port: 443,
         secure: true,
         name: "MilloHost"
     },
     {
         host: "lavalink-v4.triniumhost.com",
-        password: "https://discord.gg/CxsBjs5RAU",
+        password: "free",
         port: 443,
         secure: true,
         name: "TriniumHost"
